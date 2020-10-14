7b19fa83894641d72260bd062aabdc14bbd91b0b
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-14 07:29:48 +0530

    Till tst cases 3.2

---
+++ MoodAnalyse.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzerProgram
+{
+   public class MoodAnalyse
+    {
+        private string message;
+        public MoodAnalyse(string message)
+        {
+            this.message = message;
+        }
+        public string Analyse()
+        {
+            //try
+            //{
+            //    if (this.message.contains("sad"))
+            //        return "sad";
+            //    else
+            //        return "happy";
+            //}
+            //catch
+            //{
+            //    return "happy";
+            //}
+
+            try
+            {
+                if(this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if(this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch(NullReferenceException)
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+
+            }
+        }
+    }
+}
